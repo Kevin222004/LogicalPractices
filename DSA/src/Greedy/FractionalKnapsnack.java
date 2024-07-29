17facdf15a0c5c7c7a21972dc662a48ed363c507
Kevin222004 <97679350+Kevin222004@users.noreply.github.com>
2024-07-29 04:22:07 +0000

    Greedy - more example

--- DSA/src/Greedy/FractionalKnapsnack.java
+++ DSA/src/Greedy/FractionalKnapsnack.java
@@ -1,7 +1,52 @@
 package Greedy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FractionalKnapsnack {
     public static void main(String[] args) {
-        
+        int[] weight = {10, 20, 30};
+        int[] value = {60, 100, 120};
+        double capacity = 50;
+        int items = 3;
+
+        ArrayList<pair> pair = new ArrayList<>();
+
+        for (int i = 0; i < items; i++) {
+            pair.add(new pair(weight[i], value[i]));
+        }
+
+        Collections.sort(pair,  (p1, p2) -> Double.compare(((1.0) *(p2.value/p2.weight)), ((1.0) * (p1.value/p1.weight))));
+        // for (int i = 0; i < items; i++) {
+        //     System.out.println(pair.get(i).value);
+        // }
+
+        double valueInBag = 0;
+        for (int i = 0; i < items; i++) {
+            if (capacity == 0) {
+                return;
+            }
+            if (pair.get(i).weight <= capacity) {
+                capacity = capacity - pair.get(i).weight;
+                valueInBag += pair.get(i).value;
+            }
+            else {
+                double ratio = (((1.0)*pair.get(i).value)/pair.get(i).weight);
+                 valueInBag +=  ratio * capacity;
+                 capacity = 0;
+            }
+        }
+
+        System.out.println(valueInBag);
     }   
+
+    static class pair {
+        int weight;
+        int value;
+        
+        pair(int w, int v) {
+            weight = w;
+            value = v;
+        }
+    }
 }
--- DSA/src/Greedy/choclate.java
+++ DSA/src/Greedy/choclate.java
@@ -1,5 +1,36 @@
-package Greedy;
+/*
+ 
+Array represent the number of chocolate in n different packet
 
+their are m student 
+
+distribute the choclates to m stident such that every student get exactly 1 packet
+and difference between max of chocolate given to a student && minimum np of cocolate given to a student 
+is minimum
+
+ */
+
+package Greedy;
+import java.util.*;
 public class choclate {
+    public static void main(String[] args) {
+        int n = 8; int m = 5;
+        int[] choclates = {3, 4, 1, 9, 56, 7, 9, 12};
     
+        int mini = Integer.MAX_VALUE;
+        
+        Arrays.sort(choclates);
+        
+        int start = 0;
+        int end = m-1;
+
+        while (end < choclates.length) {
+            int diff = choclates[end] - choclates[start];
+            mini = Math.min(mini, diff);
+            start++;
+            end++;
+        }        
+
+        System.out.println(mini);
+    }
 }
